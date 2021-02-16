c57d43e44f681d0194c230c7744a6f0ceb348377
Henry122702 <henryjoestar@gmail.com>
2021-02-16 19:38:22 +0700

    StrukData-SBD-PerancanganWeb
    sisa CSS and design

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 
 
@@ -10,36 +11,31 @@ const app = express();
 //connect to mongodb
 const dbuRI = 'mongodb+srv://user-1:test@clusterstrukdata-sbdsem.ewtfk.mongodb.net/StrukData-SBD-sem2?retryWrites=true&w=majority';
 mongoose.connect(dbuRI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
- .then((result) => console.log('connected to db'))
+ .then((result) => app.listen(3000))
  .catch((err) => console.log(err))
 
-// listen for requests
-app.listen(3000);
 
-//middleware & static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
+
 
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
 
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded ({ extended: true}));
+app.use(morgan('dev'));
+
+//routes 
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+//blog Routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
